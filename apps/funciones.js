2111401f551bbbb16fe6281cd13bc9ce3ca4a37d
german pogliani <gerdarpog@gmail.com>
2023-04-24 10:15:05 -0300

    mejoras en las funciones, estilo e index.html

--- apps/funciones.js
+++ apps/funciones.js
@@ -15,7 +15,7 @@
 */
 
 
-let _encripta = {"e": "enter", "i": "imes", "o": "ober", "u": "ufat", "a": "ai"};
+let _encripta = { "e": "enter", "i": "imes", "a": "ai", "o": "ober", "u": "ufat" };
 
 
 /**
@@ -69,11 +69,90 @@ function desencriptar() {
 
 /**
  * 
+ * encripta y desencripta segun una opcion
+ * 
+ */
+//document.addEventListener("DOMContentLoaded", function () { 
+function codificar(opcion) {
+    // capturamos el texto;
+    let _texto = document.getElementById("codex").value;    
+
+    //evaluamos si hay datos.
+    if (_texto === "") {
+        // mostramos la imagen de no result.    
+        document.getElementById("imagen1").style.display = "block";
+        document.getElementById("resultado").style.display = "none";
+
+        // ocultamos el boton copiar.
+        //document.getElementById("boton_copy").style.visibility = "visible";
+        document.getElementById("nodato").style.display= "inherit";
+
+        swal("Oops!", "Algo salió mal en la página!", "error");
+
+    } else if (opcion === 1) {
+        // recorremos el diccionario a traves de sus claves.
+        // codifico
+        for (var i in _encripta) {
+            _texto = _texto.replaceAll(i, _encripta[i]);
+        }
+
+        // ocultamos la imagen.
+        document.getElementById("nodato").style.display= "none";
+
+        // mostramos el panel.        
+        document.getElementById("resultado").style.display = "block";
+
+        // mostramos el texto resultante.
+        document.getElementById("decodex").innerHTML = _texto;
+
+        // mostramos el boton de copiar.
+        //document.getElementById("boton-copy").style.visibility = "visible";
+        //document.getElementById("boton-copy").style.display = "show";
+        document.getElementById("boton-copy").style.display = "inherit";
+
+    } else {
+
+        // recorremos el diccionario a traves de sus claves.
+        // decodifico
+        for (var i in _encripta) {
+            _texto = _texto.replaceAll(_encripta[i], i);
+        }
+
+        // ocultamos la imagen.
+        document.getElementById("nodato").style.display= "none";
+
+        // mostramos el panel.        
+        document.getElementById("resultado").style.display = "inherit";
+
+        // mostramos el texto resultante.
+        document.getElementById("decodex").innerHTML = _texto;
+
+        // mostramos el boton de copiar.        
+        //document.getElementById("boton-copy").style.display = "block";
+        //document.getElementById("boton-copy").style.display = "show";
+        document.getElementById("boton-copy").style.display = "inherit";
+
+    }
+}
+
+
+/**
+ * 
  * Copia el texto encriptado o desencriptado
  * 
  */
+
+document.getElementById("boton_copy").addEventListener("click", function () {
+    //document.getElementById("boton_copy").style.display = "show";
+    document.getElementById("boton_copy").style.visibility="visible";
+    swal("Excelente!", "Se copió el texto al portapapeles!", "info");
+});
+
+/*
 function copiar_texto() {
-    alert("copiar texto");
+    //alert("copiar texto");
+    swal("Oops!", "Something went wrong on the page!", "error");
 }
+*/
 
 
